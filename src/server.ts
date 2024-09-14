114cc0270adbf1614fa0fa1a368b64046a203a58
centomila <26800547+centomila@users.noreply.github.com>
2024-09-14 23:36:27 +0200

    Refactored with TS and Webpack

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from "express";
-import { WebMidi } from "webmidi";
 
 const app = express();
 const port = 3000;
@@ -10,24 +9,7 @@ app.get("/", (req: Request, res: Response) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-WebMidi
-  .enable()
-  .then(() => {
-    console.log("WebMIDI enabled");
 
-    const inputs = WebMidi.inputs;
-    inputs.forEach((input) => {
-      console.log(`Input device: ${input.name}`);
-    });
-
-    const output = WebMidi.outputs[0];
-    if (output) {
-      output.sendActiveSensing();
-    }
-  })
-  .catch((err: Error) => {
-    console.error("WebMIDI could not be enabled:", err);
-  });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
