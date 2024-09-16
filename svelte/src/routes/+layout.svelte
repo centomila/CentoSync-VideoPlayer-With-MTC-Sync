b543f245534ac106f167bac887b2b723bb6d57a3
centomila <26800547+centomila@users.noreply.github.com>
2024-09-16 19:24:01 +0200

    layout is in layout.svelte now

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -1,5 +1,40 @@
 <script>
 	import '../app.css';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+	import MidiPortSelection from './MidiPortSelection.svelte';
+	import MidiTimersCheckboxMTC from './MidiTimersCheckboxMTC.svelte';
+	import MidiTimersCheckboxSPP from './MidiTimersCheckboxSPP.svelte';
+	import Footer from './Footer.svelte';
 </script>
 
-<slot></slot>
+<div class="flex min-h-screen flex-col">
+	<header>
+		<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+			<svelte:fragment slot="lead">
+				<span class="text-lg">MTC Display</span>
+			</svelte:fragment>
+			<svelte:fragment slot="default">
+				<div class="flex items-center space-x-8">
+					<MidiPortSelection />
+					<MidiTimersCheckboxMTC />
+					<MidiTimersCheckboxSPP />
+				</div>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<div class="flex items-center justify-end">
+					<LightSwitch />
+				</div>
+			</svelte:fragment>
+		</AppBar>
+	</header>
+
+	<main class="flex flex-grow items-center justify-center">
+		<slot></slot>
+		<!-- Page content will be injected here -->
+	</main>
+
+	<footer class="footer bottom-0 w-full bg-gray-950 py-5 text-center">
+		<Footer />
+	</footer>
+</div>
--- svelte/src/routes/Footer.svelte
+++ svelte/src/routes/Footer.svelte
@@ -1,11 +1,9 @@
-<footer class="footer  bottom-0 w-full bg-gray-950 py-5 text-center">
-	<div id="copyright" class="text-xs">
-		<p class="font-semibold">
-			Made with ❤️ by <a
-				target="_blank"
-				href="https://centomila.com"
-				class="text-blue-500 hover:underline">Centomila</a
-			>
-		</p>
-	</div>
-</footer>
+<div id="copyright" class="text-xs">
+	<p class="font-semibold">
+		Made with ❤️ by <a
+			target="_blank"
+			href="https://centomila.com"
+			class="text-blue-500 hover:underline">Centomila</a
+		>
+	</p>
+</div>
