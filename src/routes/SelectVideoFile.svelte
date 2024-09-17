3c19f234aabbd31f786ea7e8fd2dd9694b968e00
centomila <26800547+centomila@users.noreply.github.com>
2024-09-17 18:04:08 +0200

    drag and drop ok

--- src/routes/SelectVideoFile.svelte
+++ src/routes/SelectVideoFile.svelte
@@ -1,14 +1,69 @@
-<script>
+<script lang="ts">
 	import { FileButton } from '@skeletonlabs/skeleton';
-    import { FileDropzone } from '@skeletonlabs/skeleton';
+	import { FileDropzone } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
 
-    let files = [];
+	let files: FileList;
+	let fileName = 'Load a video file or drag and drop it on the page';
+
+	let dropZone: HTMLElement;
+	onMount(() => {
+		dropZone = document.getElementById('drop-zone')!;
+		initDropZone();
+	});
+
+	function preventDefaults(event: Event) {
+		event.preventDefault();
+		event.stopPropagation();
+	}
+
+	function initDropZone() {
+		// Prevent default behavior when files are dragged over or dropped
+		['dragenter', 'dragover', 'dragleave', 'drop'].forEach((eventName) => {
+			dropZone?.addEventListener(eventName, preventDefaults, false);
+		});
+
+		// Highlight drop zone when dragging over it
+		['dragenter', 'dragover'].forEach((eventName) => {
+			dropZone?.addEventListener(
+				eventName,
+				() => {
+					dropZone.classList.add('dragover');
+				},
+				false
+			);
+		});
+
+		// Remove highlight on drag leave or drop
+		['dragleave', 'drop'].forEach((eventName) => {
+			dropZone?.addEventListener(
+				eventName,
+				() => {
+					dropZone.classList.remove('dragover');
+				},
+				false
+			);
+		});
+
+		dropZone.addEventListener('drop', (event) => {
+			if (event.dataTransfer) {
+				files = event.dataTransfer.files;
+			}
+      loadVideoFile();
+		});
+	}
+
+	function loadVideoFile() {
+		if (files.length > 0) {
+			console.log(files);
+		}
+    fileName = files[0].name;
+	}
 </script>
 
-<!-- <FileButton name="files" fileInputType="video" width="w-64" class="leading-none">
-  <i class="fas fa-lg fa-video leading-none" ></i>
-  <span class="relative" style="bottom: 1px">Select Video</span>
-</FileButton>
- -->
-
-<FileDropzone name="files" fileInputType="video" padding="p-3" ><i class="fas fa-lg fa-video leading-none" ></i><span class="relative" style="bottom: 1px">Select Video</span></FileDropzone>
+<button class="btn variant-filled-primary">
+	<label for="video-file" class="flex cursor-pointer items-center space-x-2">
+		<i class="fa fa-film"></i><span style="translate: 0 -0.095rem">{fileName}</span>
+		<input type="file" id="video-file" bind:files accept="video" class="hidden" />
+	</label>
+</button>
