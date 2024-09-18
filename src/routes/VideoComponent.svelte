c70195f6b23cf5abb70b7b4c05ad6fcfd8f77abe
centomila <26800547+centomila@users.noreply.github.com>
2024-09-18 18:23:10 +0200

    player file loading ok

--- src/routes/VideoComponent.svelte
+++ src/routes/VideoComponent.svelte
@@ -1,32 +1,63 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onMount, onDestroy } from 'svelte';
 	import videojs from 'video.js';
+	import type Player from 'video.js/dist/types/player';
+	import { loadedFiles } from '$lib/stores';
 
-	const videoFileTestPath = './Question-Mark.mp4';
-  
+	$: $loadedFiles;
+
+
+	const defaultVideo = './Question-Mark.mp4';
+
+	let player: Player | null = null;
+	let videoElement: HTMLVideoElement;
+
+	function updatePlayer() {
+		if (player && $loadedFiles.files?.[0]) {
+			player.src({
+				src: URL.createObjectURL($loadedFiles.files[0]),
+				type: $loadedFiles.files[0].type
+			});
+			console.log($loadedFiles.files[0].type);
+		}
+	}
+
+	$: $loadedFiles, updatePlayer();
+
+	function initializePlayer() {
+		if (videoElement && !player) {
+			player = videojs(videoElement, {
+				autoSetup: true,
+				width: window.parent.innerWidth / 1.2,
+				height: window.parent.innerHeight / 1.2,
+				enableSmoothSeeking: true,
+				responsive: true,
+				controls: true,
+				muted: true,
+				sources: [
+					{
+						src: $loadedFiles.files?.[0] ? URL.createObjectURL($loadedFiles.files[0]) : defaultVideo,
+						type: $loadedFiles.files?.[0]?.type || 'video/mp4'
+					}
+				]
+			});
+		}
+	}
+
 	onMount(() => {
-    
-		const player = videojs('my-video', {
-			autoSetup: true,
-      width: window.parent.innerWidth / 1.2,
-      height: window.parent.innerHeight / 1.2,
-			enableSmoothSeeking: true,
-			fluid: false,
-			// aspectRatio: '16:9',
-			responsive: true,
-			controls: true,
-			muted: true,
-			sources: [
-				{
-					src: videoFileTestPath,
-					type: 'video/mp4'
-				}
-			]
-		});
+		initializePlayer();
+	});
+
+	onDestroy(() => {
+		if (player && typeof player.dispose === 'function') {
+			player.dispose();
+			player = null;
+		}
 	});
 </script>
 
 <section class="block">
 	<!-- svelte-ignore a11y-media-has-caption -->
-	<video id="my-video" class="video-js"  preload="auto"></video>
+	<video bind:this={videoElement} id="my-video" class="video-js" preload="auto">
+	</video>
 </section>
